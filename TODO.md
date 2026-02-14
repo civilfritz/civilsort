0de5b4e45c97ce15a08a51ea49cc8f5ea9d044fb
Jonathon Anderson <janderson@ciq.com>
2026-02-13 18:54:30 -0700

    Better handling of /about
    Signed-off-by: Jonathon Anderson <janderson@ciq.com>

--- TODO.md
+++ TODO.md
@@ -4,3 +4,7 @@
 - Deployment / packaging with systemd service
 - Github automation
 - configurable database location
+- back to ballot from /about
+- create a new ballot
+- number of participants
+- bug report on GitHub
--- internal/handler/about.go
+++ internal/handler/about.go
@@ -7,7 +7,10 @@ import (
 
 // HandleAbout renders the about page.
 func (h *Handler) HandleAbout(w http.ResponseWriter, r *http.Request) {
-	if err := h.templates.ExecuteTemplate(w, "about.html", nil); err != nil {
+	data := map[string]interface{}{
+		"FromBallot": r.URL.Query().Get("from"),
+	}
+	if err := h.templates.ExecuteTemplate(w, "about.html", data); err != nil {
 		log.Printf("Error rendering about: %v", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
